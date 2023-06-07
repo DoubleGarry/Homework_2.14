ce3552567fff2fb9530e8a3802400adf43ad01ba
semenikhin <garry@nextmail.ru>
2023-06-07 22:43:28 +0300

    Done

---
+++ src/main/java/com/example/homework_2_14_algorithms1/exceptions/InvalidItemException.java
@@ -0,0 +1,4 @@
+package com.example.homework_2_14_algorithms1.exceptions;
+
+public class InvalidItemException extends RuntimeException {
+}
---
+++ src/test/java/com/example/homework_2_14_algorithms1/Homework214Algorithms1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homework_2_14_algorithms1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework214Algorithms1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
